6f1221146a807104e06c5a40ed1427a4548ac6f8
Celine <cean0002@stud.kea.dk>
2025-01-07 11:41:13 +0100

    tilføjet java

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// Funktion til at starte animationen af MacMiller-figuren
+function startMacMillerFall() {
+  // Find containeren og sprite-elementet
+  const macmillerContainer = document.querySelector("#macmiller_container");
+
+  // Tilføj animation class til at få figuren til at falde ned
+  macmillerContainer.classList.add("falling");
+
+  // Når animationen er færdig, skal vi ændre dens position
+  macmillerContainer.addEventListener("animationend", function () {
+    macmillerContainer.style.top = "123%"; // Stop figuren i midten af skærmen
+    macmillerContainer.classList.remove("falling"); // Fjern animationen
+  });
+}
+
+// Eventlistener for at starte animationen når siden er loadet
+window.addEventListener("load", function () {
+  startMacMillerFall();
+});
